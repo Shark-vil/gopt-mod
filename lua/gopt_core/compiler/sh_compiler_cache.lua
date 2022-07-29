f1fc076e2f74263a0f2ebd11ea205d7dc136862e
Shark-vil <shark_vil@mail.ru>
2022-07-29 03:05:58 +0300

    Script optimization does not work well without the necessary API

--- lua/gopt_core/compiler/sh_compiler_cache.lua
+++
@@ -1,4 +0,0 @@
-hook.Add('GOpt.ScriptInclude', 'GOpt.ScriptsCacheWriter', function(lua_object, success)
-	GOptCore.Api.WriteCahce(lua_object)
-	if not success then GOptCore.Api.WriteErrorCahce(lua_object) end
-end)
