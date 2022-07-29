2cadc596110c6d92afa0bc0485993277c0827de6
Shark-vil <shark_vil@mail.ru>
2022-07-29 03:16:56 +0300

    Update sh_widgets.lua

--- lua/gopt_core/system_optimization/sh_widgets.lua
+++ lua/gopt_core/system_optimization/sh_widgets.lua
@@ -4,17 +4,3 @@ hook.Add('PreGamemodeLoaded', 'GOpt.WidgetsRemover', function()
 	function widgets.PlayerTick() end
 	hook.Remove('PlayerTick', 'TickWidgets')
 end)
-
--- hook.Add('OnEntityCreated', 'GOpt.BackWidgets',function(ent)
--- 	if ent:IsWidget() then
--- 		MsgN('Fucked widgets back')
-
--- 		-- widgets.PlayerTick = GOptCore.Storage.WidgetsPlayerTick
-
--- 		-- hook.Add('PlayerTick', 'TickWidgets', function(pl, mv)
--- 		-- 	widgets.PlayerTick(pl, mv)
--- 		-- end)
-
--- 		hook.Remove('OnEntityCreated','BackWidgets')
--- 	end
--- end)
